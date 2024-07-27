62c6d0858b930cf8fdc709aeb51cfb55bc93c872
Luis Bustamante <luissebastianbustamante@gmail.com>
2024-07-27 17:33:02 -0300

    Se agrega congiruracion de TypeORM para la conexion con la BD

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: '',
+      password: '',
+      database: '',
+      entities: [],
+      synchronize: true,
+    }),
+    UsersModule,
+  ],
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService],
 })
 export class UsersModule {}
