e020a955170b20b781ef14d2173eb4e39e0766cc
Luis Bustamante <luissebastianbustamante@gmail.com>
2024-07-28 18:25:40 -0300

    Se agregan funciones, createUser, getUsers, getUser(id), deleteUser(id) y deleteUser(id)

--- src/app.module.ts
+++ src/app.module.ts
@@ -13,7 +13,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       username: 'root',
       password: '23111207',
       database: 'nestdb',
-      entities: [__dirname + '/**/*/.entity.ts'],
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
     UsersModule,
